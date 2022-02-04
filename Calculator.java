15f5677c2e873507f8b1b3430b7a75c98ec6cb92
Aaron Shank <47933546+aaronshank@users.noreply.github.com>
2022-02-04 14:47:34 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,55 +1,65 @@
-
 import java.util.Scanner;
+
+// Interface for lambdas
+interface Calculate {
+    int run(int x, int y);
+}
 
 public class Calculator {
 
-    public static int add(int x, int y) {
-        return x + y;
-    }
-
-    public static int subtract(int x, int y) {
-        return x - y;
-    }
-
-    public static int multiply(int x, int y) {
-        return x * y;
-    }
-
-    public static int divide(int x, int y) {
-        return x / y;
+    // Can use this for any 2 number operation
+    public static void doMath(int x, int y, Calculate calcFunction) {
+        int result = calcFunction.run(x, y);
+        System.out.println("Your value is " + result);
     }
 
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to my calculator! Please enter the first integer you'd like to calculate.");
-        int firstVal = scan.nextInt();
-        System.out.println("Please enter the next integer you'd like to calculate.");
-        int secVal = scan.nextInt();
-        System.out.println("What operation would you like to use (add, subtract, multiply, divide)?");
-        scan.nextLine();
-        String op = scan.nextLine().toLowerCase();
         boolean done = false;
+
+        // Some math lambdas
+        Calculate add = (x, y) -> x + y;
+        Calculate sub = (x, y) -> x - y;
+        Calculate mult = (x, y) -> x * y;
+        Calculate div = (x, y) -> x / y;
+
         while (done == false) {
+
+            // Moved this inside the while loop for repetition
+            System.out.println("Welcome to my calculator! Please enter the first integer you'd like to calculate.");
+            int firstVal = scan.nextInt();
+            System.out.println("Please enter the next integer you'd like to calculate.");
+            int secVal = scan.nextInt();
+            System.out.println("What operation would you like to use (add, subtract, multiply, divide)?");
+            scan.nextLine();
+            String op = scan.nextLine().toLowerCase();
+
             switch (op) {
                 case "add":
-                    System.out.println("Your value is " + add(firstVal, secVal));
+                    doMath(firstVal, secVal, add);
                     done = true;
                     break;
                 case "subtract":
-                    System.out.println("Your value is " + subtract(firstVal, secVal));
+                    doMath(firstVal, secVal, sub);
                     done = true;
                     break;
                 case "multiply":
-                    System.out.println("Your value is " + multiply(firstVal, secVal));
+                    doMath(firstVal, secVal, mult);
                     done = true;
                     break;
-                case "divide":
-                    System.out.println("Your value is " + divide(firstVal, secVal));
-                    done = true;
+                case "divide": // Added divide by 0 check
+                    if (secVal != 0) {
+                        doMath(firstVal, secVal, div);
+                        done = true;
+                    } else {
+                        System.out.println("Cannot divide by zero. Please try again.");
+                    }
                     break;
                 default:
                     System.out.println("Sorry! Please enter a valid operation");
-                    op = scan.nextLine().toLowerCase();
+                    // Commenting out since it will rerun dialog
+                    // op = scan.nextLine().toLowerCase();
             }
         }
 
